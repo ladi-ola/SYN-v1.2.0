9ab5cbfb8a564f5e67ede764a1f256a910d52857
ladioredola@gmail.com <lxdi@LadiOre.localdomain>
2026-07-03 04:24:38 -0500

    Configure production deployment

--- backend/app/main.py
+++ backend/app/main.py
@@ -40,7 +40,9 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "https://syn-v1-2-0.vercel.app/"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
